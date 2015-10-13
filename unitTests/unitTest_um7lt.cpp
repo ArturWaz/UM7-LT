114a8b9825d4e6250079668a700a6d2449eaf229
geox <arturwaz11@gmail.com>
2015-10-13 11:22:10 +0200

    prepare to unit test

---
+++ unitTests/unitTest_um7lt.cpp
@@ -0,0 +1,36 @@
+//
+// Created by geox on 12.10.15.
+//
+
+
+
+#include "../um7lt/packetSplitter_um7lt.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE um7lt
+#include <boost/test/unit_test.hpp>
+
+
+
+class UnitTest_um7lt {
+
+};
+
+
+
+BOOST_FIXTURE_TEST_SUITE(UM7LT,UnitTest_um7lt)
+
+
+    BOOST_AUTO_TEST_CASE(Test_PacketSplitter_um7lt){
+
+    }
+
+
+    BOOST_AUTO_TEST_CASE(Test_um7lt){
+
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
